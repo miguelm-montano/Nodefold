97f12e57065ea97a5d44288db59fc5880073ef1e
miguelm-montano <miiguel2m@gmail.com>
2026-02-21 12:46:54 +0100

    Installation and implementation of Blade Heroicons

--- app/Http/Controllers/FolderController.php
+++ app/Http/Controllers/FolderController.php
@@ -24,13 +24,22 @@ class FolderController extends Controller {
     }
 
     public function store(Request $request) {
-
+        
         $validated = $request->validate([
             'name' => 'required|string|max:50',
             'parent_id' => 'nullable|exists:folders,id'
         ]);
 
-        $folder = Auth::user()->folders()->create([
+        if ($validated['parent_id'] ?? false) {
+
+            $parent = Folder::where('id', $validated['parent_id'])->where('user_id', Auth::id())->firstOrFail();
+        
+            if ($parent->parent_id !== null) {
+                abort(403, 'Only one nesting level allowed');
+            }
+        }
+
+        Auth::user()->folders()->create([
             'name' => $validated['name'],
             'parent_id' => $validated['parent_id'] ?? null
         ]);
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,26 +1,34 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+  <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-white dark:bg-gray-900">
+  <!-- Fonts -->
+  <link rel="preconnect" href="https://fonts.bunny.net">
+  <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-    </body>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
+    rel="stylesheet">
+
+  <!-- Scripts -->
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+
+<body class="font-sans antialiased">
+  <div class="min-h-screen bg-white">
+
+    <!-- Page Content -->
+    <main>
+      {{ $slot }}
+    </main>
+  </div>
+</body>
+
 </html>
