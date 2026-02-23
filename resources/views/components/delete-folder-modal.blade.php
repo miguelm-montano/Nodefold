36bb5bf732be9c2954455134b17e5af51b1ae1c2
miguelm-montano <miiguel2m@gmail.com>
2026-02-23 13:25:04 +0100

    Changes on the delete modal

--- resources/views/components/delete-folder-modal.blade.php
+++ resources/views/components/delete-folder-modal.blade.php
@@ -4,20 +4,24 @@
   <div class="absolute inset-0 bg-black/50" @click="showDeleteModal = false"></div>
 
   <!-- Modal -->
-  <div class="relative bg-white p-5 rounded-xl shadow-lg w-80">
+  <div class="relative bg-white px-16 py-6 rounded-xl shadow-lg flex flex-col items-center text-center">
 
-    <h2 class="text-lg font-semibold mb-4">
-      Are you sure?
+    <span>
+      <x-heroicon-o-trash class="w-14 h-14 mb-5 text-red-500" style="stroke-width: 1" />
+    </span>
+    <h2 class="text font-semibold ">
+      You want to delete this folder?
     </h2>
 
-    <p class="text-sm text-gray-500 mb-6">
-      This folder and all resources will be permanently removed.
+    <p class="text-sma text-gray-500 mb-6">
+      This folder and all resources will<br>be permanently removed.
     </p>
 
-    <div class="flex justify-end gap-3">
+    <div class="flex justify-end gap-2">
 
       <!-- Cancel -->
-      <button type="button" @click="showDeleteModal = false" class="px-3 py-1 bg-gray-200 rounded hover:bg-gray-300">
+      <button type="button" @click="showDeleteModal = false"
+        class="px-5 py-1 bg-gray-200 hover:bg-gray-300 rounded-3xl">
         Cancel
       </button>
 
@@ -26,7 +30,7 @@
         @csrf
         @method('DELETE')
 
-        <button type="submit" class="px-3 py-1 bg-black text-white rounded hover:bg-red-600">
+        <button type="submit" class="px-5 py-1 bg-black text-white hover:bg-red-600 rounded-3xl">
           Delete
         </button>
       </form>
